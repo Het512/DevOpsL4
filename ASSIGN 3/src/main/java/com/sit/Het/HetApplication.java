09c4a95f206764badcb3673c90f6c2c700618b06
Het Shah <hetshahs6481@gmail.com>
2025-11-02 19:52:07 +0530

    Added HetApplication and its corresponding test class

--- ASSIGN 3/src/main/java/com/sit/Het/HetApplication.java
+++ ASSIGN 3/src/main/java/com/sit/Het/HetApplication.java
@@ -1,13 +1,13 @@
-package com.sit.Anushree;
+package com.sit.Het;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnushreeApplication {
+public class HetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnushreeApplication.class, args);
+		SpringApplication.run(HetApplication.class, args);
 	}
 
 }
--- ASSIGN 3/src/test/java/com/sit/Het/HetApplicationTests.java
+++ ASSIGN 3/src/test/java/com/sit/Het/HetApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AnushreeApplicationTests {
+class HetApplicationTests {
 
 	@Test
 	void contextLoads() {
